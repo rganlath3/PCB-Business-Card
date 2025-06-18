2ebcd899434f39a5f2e4e3eb37f493a28b154950
rganlath3 <ranil.ganlath@gmail.com>
2025-06-18 09:32:34 -0600

    Initial Test Scripts created

---
+++ MCU_Programming/Testing_Scripts/4_Looping_Image_Display/4_Looping_Image_Display.ino
@@ -0,0 +1,29 @@
+/*
+  By Ranil Ganlath
+  This is for testing displaying looping images or GIFs with the IPS Display and XIAO nRF52840 Sense
+
+  XIAO D0 - Pushbutton, Down
+  XIAO D1 - Pushbutton, Up
+  XIAO D2 - Pushbutton, Select
+  XIAO D3 - Pushbutton, Menu/Back
+  XIAO D4 - Battery Sense, (4.3V = 2.96V, 2.8V = 1.93V)
+  XIAO D5 - Display, BLK (Backlight)
+  XIAO D6 - Display, CS (Chip Select)
+  XIAO D7 - Display, RES (Reset)
+  XIAO D8 - Display, SCL (CLK / SCK)
+  XIAO D9 - Display, DC (Data Command)
+  XIAO D10 - Display, SDA (MOSI)
+  
+  IDE Configuration:
+  TBD
+*/
+
+void setup(void) {
+
+  
+  }
+
+void loop() {
+
+  
+  }
